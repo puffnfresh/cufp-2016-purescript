a32cd30b6d900de0a041f1e6a38de078319a3f65
Rick Winfrey <rewinfrey@github.com>
2016-09-23 16:23:14 +0900

    ICFP - purescript workshop 2

--- src/Main.purs
+++ src/Main.purs
@@ -6,33 +6,65 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Pux (renderToDOM, Update, start)
+import Data.Foldable (traverse_)
+import Data.List ((:), List(Nil))
+import Data.Tuple (Tuple(Tuple))
+import Pux (App, renderToDOM, Update, start)
 import Pux.Html.Events (onClick)
-import Signal.Channel (CHANNEL)
+import Signal (map2, sampleOn, runSignal)
+import Signal.Channel (subscribe, send, CHANNEL)
 import Prelude hiding (div)
 
 data Input = Click
            | Tick
 
-update :: forall e. Update Int Input e
-update Click state = { state: state + 1, effects: [ ] }
-update Tick state = { state: state - 1, effects: [ ] }
+type State = { score :: Int, high :: Int }
 
-view :: Int -> H.Html Input
-view score = H.div [ ] [ H.text "Score: ",
-                         H.text (show score),
-                         H.button [ onClick (\e -> Click) ] [ H.text "Click me" ]
-                       ]
+update :: forall e. Update State Input e
+update Click state =
+  let score = state.score + 1
+  in { state: { score: score, high: max score state.high }, effects: [ ] }
+update Tick state =
+  let score = max 0 (state.score - 1)
+  in { state: state { score = score }, effects: [ ] }
+
+view :: State -> H.Html Input
+view state =
+  let score = H.div [ ] [ H.text "Score: ",
+                          H.text (show state.score)
+                        ]
+      high = H.div [ ] [ H.text "High: ", H.text (show state.high) ]
+  in H.div [] [ score
+              , high
+              , H.button [ onClick (\e -> Click) ] [ H.text "Click me" ]
+              ]
+
+tick :: forall e. App State Input -> Eff (channel :: CHANNEL | e) Unit
+tick app = send app.actionChannel (Tick:Nil)
+
+onTick :: forall e. App State Input -> Input -> State -> Eff (console :: CONSOLE, channel :: CHANNEL, err :: EXCEPTION | e) Unit
+onTick app Tick state =
+  let e = liftEff $ tick app
+  in do
+    log "TICKED"
+    void (launchAff (later' (300 - (4 * state.score)) e))
+onTick _ _ _ = pure unit
+
+sendTick :: forall e. App State Input -> Tuple (List Input) State -> Eff (console :: CONSOLE, channel :: CHANNEL, err :: EXCEPTION | e) Unit
+sendTick app (Tuple inputs state) = traverse_ (\i -> onTick app i state) inputs
 
 main :: forall e. Eff (channel :: CHANNEL, err :: EXCEPTION, console :: CONSOLE | e) Unit
 main = do
   app <- start {
     update: update,
     view: view,
-    initialState: 0,
+    initialState: { score: 0, high: 0 },
     inputs: [ ]
   }
-  launchAff $ later' 300 do
-    liftEff $ log "300ms"
+  let inputs = subscribe app.actionChannel
+      inputStates = map2 Tuple inputs app.state
+      onStates = sampleOn inputs inputStates
+  tick app
+  runSignal $ map (sendTick app) onStates
   renderToDOM "#app" app.html
   log "Hello sailor!"
